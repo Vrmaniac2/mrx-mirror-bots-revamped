fa0f695abe35f9fd6d8eefadffb3034bd7f2aeec
Vrmaniac2 <98936200+Vrmaniac2@users.noreply.github.com>
2022-07-09 16:23:00 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -160,7 +160,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Z-Mirror-Bot',
+            'description': 'Uploaded-by-Mr.X-Mirror-Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -426,7 +426,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Z-Mirror-Bot",
+            "description": "Uploaded-by-Mr.X-Mirror-Bot",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
