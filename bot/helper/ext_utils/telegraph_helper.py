47f5151a474045361458f292a393b3cfbf882000
Vrmaniac2 <98936200+Vrmaniac2@users.noreply.github.com>
2022-07-09 15:58:50 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,10 +72,10 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Z-Mirror-Bot Torrent Search',
+                title = 'Mr. X Mirror/Leech Zone Search',
                 content=content
             )
         return
 
 
-telegraph=TelegraphHelper('Z-Mirror-Bot', 'https://github.com/shuvam-dawn/MLTB-Custom-UI')
+telegraph=TelegraphHelper('Mr. X Mirror/Leech Zone', 'https://t.me/+HRCIHSg6X4AyMGVl')
