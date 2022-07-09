c4245e86d460f47528c84ce02007bc89abe31599
Vrmaniac2 <98936200+Vrmaniac2@users.noreply.github.com>
2022-07-09 16:33:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,7 +66,7 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("Mirror Group 💫", "https://t.me/+Qs6841fpbYViZWNl")
-    buttons.buildbutton("Owner", "https://t.me/SupergodX")
+    buttons.buildbutton("Owner", "https://telegram.dog/SuperGodX")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
